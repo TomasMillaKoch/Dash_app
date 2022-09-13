937795c0a9ac3e39d38a7f76ca1cf88204a7f4ca
TomasMillaKoch <TomasMK@cwf-fcf.org>
2022-09-13 15:54:44 -0400

    initial launch

--- assets/dashExtensions_default.js
+++
@@ -1,7 +0,0 @@
-window.dashExtensions = Object.assign({}, window.dashExtensions, {
-    default: {
-        function0: function(feature, context) {
-            return L.circleMarker(feature.coordinates);
-        }
-    }
-});
